39b15c40b1e490b224ce8d826420c09fd49fa614
vromanyu <vromanyu@outlook.com>
2025-12-01 15:57:36 +0200

    implement NoteController and basic Spring Security configuration

---
+++ src/main/java/com/vromanyu/secure/notes/config/security/SpringSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.vromanyu.secure.notes.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration {
+
+ @Bean
+ public SecurityFilterChain customFilterChain(HttpSecurity http) throws Exception {
+  http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+  http.csrf(AbstractHttpConfigurer::disable);
+  http.httpBasic(Customizer.withDefaults())
+   .authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+  return http.build();
+ }
+}
---
+++ src/main/java/com/vromanyu/secure/notes/controller/NoteController.java
@@ -0,0 +1,53 @@
+package com.vromanyu.secure.notes.controller;
+
+import com.vromanyu.secure.notes.entity.Note;
+import com.vromanyu.secure.notes.service.NoteService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/notes", produces = "application/json")
+@RequiredArgsConstructor
+@Slf4j
+public class NoteController {
+
+ private final NoteService noteService;
+
+ private void logUserDetails(UserDetails userDetails) {
+  log.info("userDetails: {}", userDetails);
+ }
+
+ @PostMapping
+ public Note createNote(@RequestBody String content, @AuthenticationPrincipal UserDetails userDetails) {
+  logUserDetails(userDetails);
+  String username = userDetails.getUsername();
+  return noteService.createNoteForUser(username, content);
+ }
+
+ @GetMapping
+ public List<Note> getAllNotes(@AuthenticationPrincipal UserDetails userDetails) {
+  logUserDetails(userDetails);
+  String username = userDetails.getUsername();
+  return noteService.getNotesForUser(username);
+ }
+
+ @PutMapping("/{id}")
+ public Note updateNote(@PathVariable Integer id, @RequestBody String content, @AuthenticationPrincipal UserDetails userDetails) {
+  logUserDetails(userDetails);
+  String username = userDetails.getUsername();
+  return noteService.updateNoteForUser(id, content, username);
+ }
+
+ @DeleteMapping("/{id}")
+ public void deleteNote(@PathVariable Integer id, @AuthenticationPrincipal UserDetails userDetails) {
+  logUserDetails(userDetails);
+  String username = userDetails.getUsername();
+  noteService.deleteNoteForUser(id, username);
+ }
+
+}

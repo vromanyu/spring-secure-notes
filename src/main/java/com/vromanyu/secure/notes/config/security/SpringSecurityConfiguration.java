7ec5bfdb2dab883c9aa69d0a52525762be9773c6
vromanyu <vromanyu@outlook.com>
2025-12-02 14:09:03 +0200

    remove redundant configuration

--- src/main/java/com/vromanyu/secure/notes/config/security/SpringSecurityConfiguration.java
+++ src/main/java/com/vromanyu/secure/notes/config/security/SpringSecurityConfiguration.java
@@ -7,12 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -25,15 +20,6 @@ public class SpringSecurityConfiguration {
   http.httpBasic(Customizer.withDefaults())
    .authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
   return http.build();
- }
-
- @Bean
- public UserDetailsService userDetailsService(DataSource dataSource) {
-  JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
-  userDetailsManager.createUser(User.withUsername("admin").password("{noop}1234").roles("ADMIN").build());
-  userDetailsManager.createUser(User.withUsername("user1").password("{noop}1234").roles("USER").build());
-  userDetailsManager.createUser(User.withUsername("user2").password("{noop}1234").roles("USER").build());
-  return userDetailsManager;
  }
 
 }

4353bb28ba86c11e7424c3399915cf3b4f6f5709
vromanyu <vromanyu@outlook.com>
2025-12-02 15:03:09 +0200

    move the @PreAuthorize annotation on the AdminController

--- src/main/java/com/vromanyu/secure/notes/controller/AdminController.java
+++ src/main/java/com/vromanyu/secure/notes/controller/AdminController.java
@@ -5,6 +5,7 @@ import com.vromanyu.secure.notes.entity.AppUser;
 import com.vromanyu.secure.notes.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
  private final UserService userService;
--- src/main/java/com/vromanyu/secure/notes/service/UserService.java
+++ src/main/java/com/vromanyu/secure/notes/service/UserService.java
@@ -2,15 +2,11 @@ package com.vromanyu.secure.notes.service;
 
 import com.vromanyu.secure.notes.dto.AppUserDto;
 import com.vromanyu.secure.notes.entity.AppUser;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface UserService {
- @PreAuthorize("hasRole('ADMIN')")
  void updateUserRole(Integer id, String roleName);
- @PreAuthorize("hasRole('ADMIN')")
  List<AppUser> getAllUsers();
- @PreAuthorize("hasRole('ADMIN')")
  AppUserDto getUserById(Integer id);
 }

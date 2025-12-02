8d964896e83a74f82d6c9141acebced2643d7991
vromanyu <vromanyu@outlook.com>
2025-12-02 14:06:54 +0200

    create entity AppUser (Custom User Model)

---
+++ src/main/java/com/vromanyu/secure/notes/entity/AppUser.java
@@ -0,0 +1,62 @@
+package com.vromanyu.secure.notes.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class AppUser {
+
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Integer id;
+
+ @NotBlank
+ @Column(unique = true)
+ private String username;
+
+ @NotBlank
+ @Email
+ @Column(unique = true)
+ private String email;
+
+ @JsonIgnore
+ private String password;
+
+ private boolean nonLocked = true;
+ private boolean nonExpired = true;
+ private boolean credentialsNonExpired = true;
+ private boolean enabled = true;
+
+ private LocalDate credentialsExpiryDate;
+ private LocalDate accountExpiryDate;
+
+ private String twoFactorSecret;
+ private boolean isTwoFactorEnabled = false;
+ private String signUpMethod;
+
+ @ManyToOne(cascade = CascadeType.MERGE)
+ @JoinColumn
+ @ToString.Exclude
+ private AppRole appRole;
+
+ @CreationTimestamp
+ private LocalDateTime creationDate;
+
+ @UpdateTimestamp
+ private LocalDateTime updateDate;
+}

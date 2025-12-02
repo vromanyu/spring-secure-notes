7bceb29be0d1ebdcf74870cbda5401d92615c545
vromanyu <vromanyu@outlook.com>
2025-12-02 15:39:09 +0200

    refactors

--- src/main/java/com/vromanyu/secure/notes/SecureNotesApplication.java
+++ src/main/java/com/vromanyu/secure/notes/SecureNotesApplication.java
@@ -23,38 +23,26 @@ public class SecureNotesApplication {
    AppRole userRole = appRoleRepository.findByRole(AppRoleEnum.ROLE_USER).orElseGet(() -> appRoleRepository.save(new AppRole(AppRoleEnum.ROLE_USER)));
    AppRole adminRole = appRoleRepository.findByRole(AppRoleEnum.ROLE_ADMIN).orElseGet(() -> appRoleRepository.save(new AppRole(AppRoleEnum.ROLE_ADMIN)));
 
-    if (!appUserRepository.existsByUsername("user")) {
-     AppUser appUser = new AppUser();
-     appUser.setUsername("user");
-     appUser.setEmail("user@gmail.com");
-     appUser.setPassword("{noop}1234");
-     appUser.setNonLocked(false);
-     appUser.setNonExpired(true);
-     appUser.setCredentialsNonExpired(true);
-     appUser.setEnabled(true);
-     appUser.setTwoFactorEnabled(false);
-     appUser.setSignUpMethod("email");
-     appUser.setAppRole(userRole);
-     userRole.getUsers().add(appUser);
-     appUserRepository.save(appUser);
-    }
-
-   if (!appUserRepository.existsByUsername("admin")) {
-    AppUser adminUser = new AppUser();
-    adminUser.setUsername("admin");
-    adminUser.setEmail("admin@gmail.com");
-    adminUser.setPassword("{noop}1234");
-    adminUser.setNonLocked(false);
-    adminUser.setNonExpired(true);
-    adminUser.setCredentialsNonExpired(true);
-    adminUser.setEnabled(true);
-    adminUser.setTwoFactorEnabled(false);
-    adminUser.setSignUpMethod("email");
-    adminUser.setAppRole(adminRole);
-    adminRole.getUsers().add(adminUser);
-    appUserRepository.save(adminUser);
-   }
+   generateUser(appUserRepository, "user", "user@gmail.com", userRole);
+   generateUser(appUserRepository, "admin", "admin@gmail.com", adminRole);
   };
  }
 
+ private static void generateUser(AppUserRepository appUserRepository, String user, String mail, AppRole userRole) {
+  if (!appUserRepository.existsByUsername(user)) {
+   AppUser appUser = new AppUser();
+   appUser.setUsername(user);
+   appUser.setEmail(mail);
+   appUser.setPassword("{noop}1234");
+   appUser.setNonLocked(false);
+   appUser.setNonExpired(true);
+   appUser.setCredentialsNonExpired(true);
+   appUser.setEnabled(true);
+   appUser.setTwoFactorEnabled(false);
+   appUser.setSignUpMethod("email");
+   appUser.setAppRole(userRole);
+   userRole.getUsers().add(appUser);
+   appUserRepository.save(appUser);
+  }
+ }
 }

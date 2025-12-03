3c5b63a31d1801bf6e897842d5b5a22d29169086
vromanyu <vromanyu@outlook.com>
2025-12-03 17:19:51 +0200

    implement CSRF protection

--- src/main/java/com/vromanyu/secure/notes/config/security/SpringSecurityConfiguration.java
+++ src/main/java/com/vromanyu/secure/notes/config/security/SpringSecurityConfiguration.java
@@ -7,10 +7,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -19,13 +20,17 @@ public class SpringSecurityConfiguration {
 
  @Bean
  public SecurityFilterChain customFilterChain(HttpSecurity http, LoggingFilter loggingFilter) throws Exception {
-  http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-  http.csrf(AbstractHttpConfigurer::disable);
+  http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+  http.csrf(c -> {
+   c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+   c.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler());
+  });
   http.addFilterBefore(loggingFilter, BasicAuthenticationFilter.class);
   http.httpBasic(Customizer.withDefaults())
    .authorizeHttpRequests(requests ->
     requests
      .requestMatchers("/public/**").permitAll()
+     .requestMatchers(("/api/csrf")).permitAll()
      .requestMatchers("/error/**").permitAll()
      .requestMatchers("/api/admin/**").hasRole("ADMIN")
      .anyRequest().authenticated());

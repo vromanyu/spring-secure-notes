9a47c1b024ead3763d73eb4cc5ec09eed1bf313a
vromanyu <vromanyu@outlook.com>
2025-12-04 15:13:10 +0200

    change CSRF controller mapping

--- src/main/java/com/vromanyu/secure/notes/controller/CsrfController.java
+++ src/main/java/com/vromanyu/secure/notes/controller/CsrfController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/csrf")
+@RequestMapping("public/api/csrf")
 public class CsrfController {
 
  @GetMapping
